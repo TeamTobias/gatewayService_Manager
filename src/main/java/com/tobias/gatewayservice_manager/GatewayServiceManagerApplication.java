2e62425069f1a5c82836f2c0de854220f503627b
DietCoding <jaejoodo1@gmail.com>
2022-11-14 23:05:34 +0900

    init commit

---
+++ src/main/java/com/tobias/gatewayservice_manager/GatewayServiceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.gatewayservice_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tobias/gatewayservice_manager/GatewayServiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.gatewayservice_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
