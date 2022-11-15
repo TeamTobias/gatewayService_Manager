383e017ac878277e84f82776a46d8f1c8bea3ac4
DietCoding <jaejoodo1@gmail.com>
2022-11-15 13:57:07 +0900

    gatewayServiceManager application.yml

--- src/main/java/com/tobias/gatewayservice_manager/GatewayServiceManagerApplication.java
+++ src/main/java/com/tobias/gatewayservice_manager/GatewayServiceManagerApplication.java
@@ -2,8 +2,10 @@ package com.tobias.gatewayservice_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayServiceManagerApplication {
 
     public static void main(String[] args) {
