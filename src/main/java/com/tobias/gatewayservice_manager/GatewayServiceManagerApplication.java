273352ac1bf82fde833437eda916729e649b09ba
DietCoding <jaejoodo1@gmail.com>
2022-12-11 12:52:48 +0900

    modify application.yml

--- src/main/java/com/tobias/gatewayservice_manager/GatewayServiceManagerApplication.java
+++ src/main/java/com/tobias/gatewayservice_manager/GatewayServiceManagerApplication.java
@@ -2,10 +2,11 @@ package com.tobias.gatewayservice_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayServiceManagerApplication {
 
     public static void main(String[] args) {
